ad02c01e2dbdd4597328f8e845d9b8653ffd5b6f
369 Research Dev <dev@369research.eu>
2026-05-25 03:02:51 +0000

    fix: upgrade Sendcloud API v2 to v3

--- server/sendcloudService.ts
+++ server/sendcloudService.ts
@@ -7,7 +7,7 @@
 import { createHmac } from "crypto";
 import { ENV } from "./env.js";
 
-const SENDCLOUD_API_URL = "https://panel.sendcloud.sc/api/v2";
+const SENDCLOUD_API_URL = "https://panel.sendcloud.sc/api/v3";
 
 interface SendcloudAddress {
   name: string;
